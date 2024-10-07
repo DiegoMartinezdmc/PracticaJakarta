3a7cb1a6b769870e40cdc853f9e876541bb42920
DmartzzC <dmartinezc@uamv.edu.ni>
2024-10-07 12:03:22 -0600

    Initial commit

---
+++ src/main/java/org/damc/practicajakarta/HelloApplication.java
@@ -0,0 +1,9 @@
+package org.damc.practicajakarta;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
